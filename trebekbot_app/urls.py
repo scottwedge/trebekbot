d50fa013e6d19037c5c4d57b4f3e68a803b67819
BertrandHustle <scott.greenberg926@gmail.com>
2020-06-15 14:07:11 -0400

    testing app.urls w/heroku pattern

--- trebekbot_app/urls.py
+++ trebekbot_app/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('trebekbot-django.herokuapp.com', views.index, name='index'),
 ]
