bec2b4e5e9846f2575b3efcaca6ac5fa25887fda
BertrandHustle <scott.greenberg926@gmail.com>
2020-06-16 12:25:34 -0400

    removing duplicate url paths

--- trebekbot_app/urls.py
+++ trebekbot_app/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('question/', views.question, name='question'),
-    path('test/', views.test, name='testpage'),
+    path('', views.question, name='question'),
+    path('', views.test, name='testpage'),
 ]
