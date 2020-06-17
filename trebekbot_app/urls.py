2dd8e17617120eec05a82b56724aeb5d94d50d8a
BertrandHustle <scott.greenberg926@gmail.com>
2020-06-17 09:11:00 -0400

    even more url pattern testing

--- trebekbot_app/urls.py
+++ trebekbot_app/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.question, name='question'),
+    # path('', views.question, name='question'),
     path('', views.test, name='testpage'),
 ]
