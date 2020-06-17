0b9ac293a7557ff37afee905b4e9ec4aa5a57109
BertrandHustle <scott.greenberg926@gmail.com>
2020-06-17 09:15:36 -0400

    specifying included view in urls.py

--- trebekbot_project/urls.py
+++ trebekbot_project/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('question/', include('trebekbot_app.urls'), name='question'),
-    path('test/', include('trebekbot_app.urls'), name='testpage'),
+    path('question/', include('trebekbot_app.urls.question'), name='question'),
+    path('test/', include('trebekbot_app.urls.test'), name='testpage'),
     path('admin/', admin.site.urls),
 ]
